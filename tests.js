a6436db14b878e968765edafb84680d4a4929c8e
cwdesautels <cwdesautels@learn.senecac.on.ca>
2010-12-04 15:00:03 -0500

    added second t2_roll -errors with c3dl

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 // These are the test files in this dir to be run.
 var tests = [
-  { name: "t1_default", model: "duck", tags: ["Static"] }
+  { name: "t1_default", model: "duck", tags: ["Static"] },
+  { name: "t2_roll", model: "duck", tags: ["Static"] }
 ];
 //"./resources/t1_default.png"
 
@@ -8,25 +9,38 @@ var tests = [
 //TEST FUNCTIONS HERE
 var testFuncs = {
   t1_default: function(canvasName){
-    // Create new c3dl.Scene object
     scn = new c3dl.Scene();
     scn.setCanvasTag(canvasName);
-    // Create GL context
     renderer = new c3dl.WebGL();
     renderer.createRenderer(this);
-    // Attach renderer to the scene
     scn.setRenderer(renderer);
     scn.init(canvasName);
     if(renderer.isReady() ){
       duck = new c3dl.Collada();
       duck.init("./resources/duck.dae");
-      // Add the object to the scene
       scn.addObjectToScene(duck);
-      // Create a camera
       var cam = new c3dl.FreeCamera();
       cam.setPosition(new Array(0, 0, -500.0));
       cam.setLookAtPoint(new Array(0.0, 0.0, 0.0));
-      // Add the camera to the scene
+      scn.setCamera(cam);
+      scn.startScene();
+    }
+  },
+  t2_roll: function(canvasName){
+    scn = new c3dl.Scene();
+    scn.setCanvasTag(canvasName);
+    renderer = new c3dl.WebGL();
+    renderer.createRenderer(this);
+    scn.setRenderer(renderer);
+    scn.init(canvasName);
+    if(renderer.isReady() ){
+      duck = new c3dl.Collada();
+      duck.init("./resources/duck.dae");
+      duck.roll(180)
+      scn.addObjectToScene(duck);
+      var cam = new c3dl.FreeCamera();
+      cam.setPosition(new Array(0, 0, -500.0));
+      cam.setLookAtPoint(new Array(0.0, 0.0, 0.0));
       scn.setCamera(cam);
       scn.startScene();
     }
