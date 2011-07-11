111d9e74f12359099f3c10488d2b2c93ad29d552
cwdesautels <carld1989@gmail.com>
2011-07-11 13:15:29 -0400

    [#53] wip

--- new.js
+++ new.js
@@ -1,122 +1,161 @@
 (function (window, undefined){
     //Global constants
     var noop = function(){},
-        epsilon = function(e){
-            return (Math.abs(e) % 101) / 100;
-        },
-        sigma = function(s){
-            return Math.abs(s);
-        },
-        isLoaded = function(obj, att) {
-            if(obj[att] && obj[att] === true){
-                return true;
+    epsilon = function(e){
+        return (Math.abs(e) % 101) / 100;
+    },
+    sigma = function(s){
+        return Math.abs(s);
+    },
+    getJson = function (src, callback) {
+        var r = new XMLHttpRequest();
+        r.open('GET', src, true);
+        r.overrideMimeType('application/json');
+        r.onerror = function () {
+            throw new Error('JSON load failed: ' + src);
+        };
+        r.onload = function () {
+            try {
+                callback(JSON.parse(r.responseText));
             }
-            return false;
-        },
-        canStart = function(obj){
-            for(var i in obj){
-                if(arr[i] === false){
-                    return false;
-                }
+            catch (e) {
+                throw new Error('JSON was not valid: ' + src);
             }
-            return true;
-        },
-        getJson = function (src, callback) {
-            var r = new XMLHttpRequest();
-            r.open('GET', src, true);
-            r.overrideMimeType('application/json');
-            r.onerror = function () {
-                throw new Error('JSON load failed: ' + src);
-            };
-            r.onload = function () {
-                try {
-                    callback(JSON.parse(r.responseText));
-                }
-                catch (e) {
-                    throw new Error('JSON was not valid: ' + src);
-                }
-            };
-            r.send(null);
-        },
-        getScript = function (src, callback) {
-            var s = window.document.createElement('script');
-            s.type = 'text/javascript';
-            s.onerror = function () {
-                throw new Error('Script load failed: ' + src);
-            };
-            s.onload = function () {
-                callback();
-                window.document.head.removeChild(s);
-            };
-            s.src = src;
-            window.document.head.appendChild(s);
-        },
-        Queue = function(num, callback){
-            var list = [], getThread = (function(n){
-                var worker = [], temp, setup = function (me) {
-                    temp = new Worker('slave.js');
-                    temp.onerror = function (event) {
-                        throw new Error('Worker Thread Error' + event.message);
-                    };
-                    temp.onmessage = function (event) {
-                        callback(event.data);
-                        var data = Queue.pop();
-                        if (data) {
-                            this.postMessage(data);
-                        }
-                        else {
-                            worker[me].status = true;
-                        }
-                    };
-                    worker.push({'worker' : temp, 'status' : true});
+        };
+        r.send(null);
+    },
+    getScript = function (src, callback) {
+        var s = window.document.createElement('script');
+        s.type = 'text/javascript';
+        s.onerror = function () {
+            throw new Error('Script load failed: ' + src);
+        };
+        s.onload = function () {
+            callback();
+            window.document.head.removeChild(s);
+        };
+        s.src = src;
+        window.document.head.appendChild(s);
+    },
+    workerQueue = function(num, callback){
+        var list = [], getThread = (function(n){
+            var worker = [], temp, setup = function (me) {
+                temp = new Worker('slave.js');
+                temp.onerror = function (event) {
+                    throw new Error('Worker Thread Error' + event.message);
                 };
-                while (n--) {
-                    setup(n);
-                }
-                return function () {
-                    var n = worker.length;
-                    while (n--) {
-                        if (worker[n].status) {
-                            worker[n].status = false;
-                            return worker[n].worker;
-                        }
+                temp.onmessage = function (event) {
+                    callback(event.data);
+                    var data = Queue.pop();
+                    if (data) {
+                        this.postMessage(data);
                     }
-                };         
-            })(num);
-            Queue.prototype.push = function (data) {
-                var worker = getThread();
-                if (worker) {
-                    worker.postMessage(data);
+                    else {
+                        worker[me].status = true;
+                    }
+                };
+                worker.push({'worker' : temp, 'status' : true});
+            };
+            while (n--) {
+                setup(n);
+            }
+            return function () {
+                var n = worker.length;
+                while (n--) {
+                    if (worker[n].status) {
+                        worker[n].status = false;
+                        return worker[n].worker;
+                    }
+                }
+            };         
+        })(num);
+        Queue.prototype.push = function (data) {
+            var worker = getThread();
+            if (worker) {
+                worker.postMessage(data);
+            }
+            else {
+                list.push(data);
+            }
+        };
+        Queue.prototype.pop = function () {
+            return list.pop();
+        };
+    },
+    functionChain = function(){
+        var Bucket = function(arr){
+            Bucket.prototype.next = undefined;
+            Bucket.prototype.data = arr;
+        }, head, tail;
+        head = tail = undefined;
+        functionChain.prototype.isEmpty = function(){
+            return head === undefined;
+        };
+        functionChain.prototype.push = function(arr){
+            var nn;
+            if(arr instanceof Array){
+                nn = new Bucket(arr);
+            }
+            else{
+                nn = new Bucket([arr]);
+            }
+            if(functionChain.isEmpty()){
+                head = tail = nn;
+            }
+            else {
+                tail.next = nn;
+                tail = nn;
+            }
+        };
+        functionChain.prototype.pop = function(){
+            if(!functionChain.isEmpty()){
+                var data = head.data;
+                if(head.next === undefined){
+                    head = tail = undefined;       
                 }
                 else {
-                    list.push(data);
+                    head = head.next;
                 }
-            };
-            Queue.prototype.pop = function () {
-                return list.pop();
-            };
+                return data;
+            }
+            else{
+                return undefined;
+            }
         };
+        functionChain.prototype.run = function(){
+            while(!functionChain.isEmpty()){
+                var bucket = functionChain.pop();
+                for(var i = 0, len = bucket.data.length; i < len; i++){
+                    bucket.data[i]();
+                }
+            }
+        };
+    };
     var Sundae = window.Sundae = function(divId){
         //Instance Variables
-        var running, ready, complete, container, queue,
-        tolerance, blur, tests, tag, files, start;
+        var running, imp, add, complete, go, 
+        container = {}, 
+        queue = new workerQueue(4, noop),
+        list = new functionChain(),
+        tolerance = 0.05,
+        blur = 2, 
+        tests = [], 
+        tag = 'all',
+        tasks = 0,
+        total = 0,
+        canStart = function(){ return tasks === 0 && tests.length === total; };
         //Initialization
-        queue = container = {};
-        tests = files = [];
-        tolerance = 5; 
-        blue = 2;
-        tag = 'all';
-        running = ready = complete = start = noop;
+        container = window.document.getElementById('divId');
+        running = imp = add = complete = go = noop;
+        //Sundae Object
         return {
             noop: noop,
             addTest: function(obj){
                 if(obj){
                     if(obj instanceof Object){
                         tests.push(obj);
-                        if(canStart(files)){
-                           start(); 
-                        }
-                        ready();
+                        add();
+                        go();
                     }
                     else {
                         throw new Error("Argument Type Mismatch: addTest expected an Object");
@@ -127,7 +166,9 @@
             addTests: function(arr){
                 if(arr){
                     if(arr instanceof Array){
-                        for(var i = 0, len = arr.length; i < len; i++){
+                        var len = arr.length;
+                        total += len;
+                        for(var i = 0; i < len; i++){
                             this.addTest(arr[i]);
                         }
                     }
@@ -140,16 +181,13 @@
             addTestFile: function(file){
                 if(file){
                     if(typeof(file) === 'string'){
-                        if(!isLoaded(files, file)){
-                            files[file] = false;
-                            getJson(file, function(){
-                                files[file] = true;
-                                if(canStart(files)){
-                                   start(); 
-                                }	
-                                this.addTests
+                        tasks += 1;
+                        list.push(function(){
+                            getJson(file, function(arr){
+                                tasks -= 1;
+                                this.addTests(arr);
                             });
-                        }
+                        });
                     }
                     else {
                         throw new Error("Argument Type Mismatch: addTestFile expected a String");
@@ -178,17 +216,13 @@
             addLibrary: function(lib){
                 if(lib){
                     if(typeof(lib) === 'string'){
-                        if(!isLoaded(files, lib)){
-                            files[lib] = false;
-                            getScript(lib, 
-                            function(){
-                                files[lib] = true;
-                                if(canStart()){
-                                   start(); 
-                                }
-                                ready();
+                        tasks += 1;
+                        list.push(function(){
+                            getScript(lib, function(){
+                                tasks -= 1;
+                                go();
                             });
-                        }
+                        });
                     }
                     else {
                         throw new Error("Argument Type Mismatch: addLibrary expected a String");
@@ -199,9 +233,17 @@
             addLibraries: function(arr){
                 if(arr){
                     if(arr instanceof Array){
-                        for(var i = 0, len = arr.length; i < len; i++){
-                            this.addLibrary(arr[i]);
+                        var libs = [], len = arr.length;
+                        tasks += len;
+                        for(var i = 0; i < len; i++){
+                            libs.push(function(){
+                                getScript(arr[i], function(){
+                                    tasks -= 1;
+                                    go();
+                                });
+                            });
                         }
+                        list.push(libs);
                     }
                     else {
                         throw new Error("Argument Type Mismatch: addLibraries expected an Array");
@@ -209,10 +251,21 @@
                 }
                 return this;
             },
-            onReady: function(callback){
+            onAdd: function(callback){
                 if(callback){
                     if(typeof(callback) === 'function'){
-                        ready = callback;
+                        add = callback;
+                    }
+                    else {
+                        throw new Error("Argument Type Mismatch: onReady expected a Function");
+                    }
+                }
+                return this;
+            },
+            onImport: function(callback){
+                if(callback){
+                    if(typeof(callback) === 'function'){
+                        imp = callback;
                     }
                     else {
                         throw new Error("Argument Type Mismatch: onReady expected a Function");
@@ -257,13 +310,13 @@
                 return tests;
             },
             start: function(){
-                queue = new Queue(4, noop);
-                if(canStart(files)){
-                    start();
-                }
-                else {
-                    start = noop;   
-                }
+                list.run();
+                go = function(){
+                    if(canStart()){
+                        //start function
+                    }
+                };
+                go();
                 return this;
             }
         };
